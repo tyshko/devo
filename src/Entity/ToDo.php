c5877a29274ab41bb4ff7700211308817dafab1a
Neitron <Neitron@inernetbs.net>
2023-11-07 11:20:06 +0200

    add new project

---
+++ src/Entity/ToDo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ToDoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ToDoRepository::class)]
+class ToDo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
